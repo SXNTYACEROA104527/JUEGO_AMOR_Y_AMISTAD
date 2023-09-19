0bbd724d944145fae41dfb0b573f7e9ba1f3c0a7
SXNTYACEROA104527 <santyacero457@hotmail.com>
2023-09-18 23:00:44 -0500

    FIRST COMMIT

---
+++ amoryamistad.py
@@ -0,0 +1,4 @@
+nombre = input("Ingresa tu nombre ")
+print("¡Hola!", nombre)
+print("¡HOY ES EL DÍA! Ya que no tienes planes en la noche, tus amig@s te invitaron a una fiesta que hará un compañero de la universidad y te contaron que tu crush va a ir, exacto al fin odrás tener la oportunidad de acercarte y poder hablarle, pero ante todo debes ir a disfrutar junto a tus amigos")
+print("")
